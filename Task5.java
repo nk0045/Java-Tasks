3c0902b413e6c679fd8ae01b6c804a6acce62df1
nk0045 <147287723+nk0045@users.noreply.github.com>
2026-02-26 12:54:10 +0530

    Add files via upload

---
+++ Task5.java
@@ -0,0 +1,16 @@
+package jhyfgd;
+import java.util.*;
+public class Task5 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of celsius : ");
+		double c = sc.nextDouble();
+		double f;
+		System.out.println("The value in fahrenheit = "+((c*1.8)+32));
+
+	}
+
+}
