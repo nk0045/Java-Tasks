2531b2c874488a59c32b8cf649bb58bd973080dd
nk0045 <147287723+nk0045@users.noreply.github.com>
2026-02-26 12:45:08 +0530

    Add files via upload

---
+++ Task3.java
@@ -0,0 +1,15 @@
+package jhyfgd;
+import java.util.*;
+public class Task3 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		float base = sc.nextFloat();
+		float height = sc.nextFloat();
+		System.out.println("The area of the Triangle = "+(0.5*base*height));
+
+	}
+
+}
