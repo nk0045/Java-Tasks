be8e79e5f33fffe9b264078273f3042dee8fba64
nk0045 <147287723+nk0045@users.noreply.github.com>
2026-02-26 13:01:48 +0530

    Add files via upload

---
+++ Task6.java
@@ -0,0 +1,15 @@
+package jhyfgd;
+import java.util.*;
+public class Task6 {
+
+	public static void main(String[] args) 
+	{
+		int a = 20;
+		int b = 10;
+		System.out.println("((a+b)^2) = "+(Math.pow(a+b, 2)));
+		System.out.println("((a-b)^2) = "+(Math.pow(a-b, 2)));
+		
+
+	}
+
+}
