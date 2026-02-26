3dd4da0156e4c3f3b4d2e5760736e6e226a78af0
nk0045 <147287723+nk0045@users.noreply.github.com>
2026-02-26 12:41:39 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,13 @@
+package jhyfgd;
+import java.util.*;
+public class Task2 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = Math.sqrt(a);
+		System.out.println("The square root of the number is :"+b);
+	}
+
+}
