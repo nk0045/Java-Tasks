766884d908583834fd167639ec1fb2a4a2e3627c
nk0045 <147287723+nk0045@users.noreply.github.com>
2026-02-26 12:49:30 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,18 @@
+package jhyfgd;
+import java.util.*;
+public class Task4 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		double kms;
+		double miles;
+	    System.out.print("Enter the value of kilometers : ");
+	    kms = sc.nextDouble();
+	    miles = kms * 0.63;
+	    System.out.print("The value in Miles = "+miles);
+
+	}
+
+}
