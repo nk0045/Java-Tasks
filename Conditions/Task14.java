0c0702e20574d69562195c9968ae33d4aedf1798
nk0045 <147287723+nk0045@users.noreply.github.com>
2026-02-28 10:24:31 +0530

    Add files via upload

---
+++ Conditions/Task14.java
@@ -0,0 +1,20 @@
+package Conditions;
+
+import java.util.Scanner;
+
+public class Task14 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the character:");
+		char a = ' ';
+		if(a == ' ')
+		{
+			System.out.println("It is a space");
+		}
+
+	}
+
+}
